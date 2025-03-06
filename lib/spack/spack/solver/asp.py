2c00fd520223f6d7c1fa348de9c149f785d9f4cc
Paul Ferrell <pferrell@lanl.gov>
2025-03-06 13:34:16 -0700

    Revert "Fixed issue with external paths relative to ."
    This reverts commit 4d64cc73d2626830f9fc6695d4a154583601e8d1.

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -2905,10 +2905,7 @@ class SpecBuilder:
         packages_yaml = spack.config.get("packages")
         packages_yaml = _normalize_packages_yaml(packages_yaml)
         spec_info = packages_yaml[node.pkg]["externals"][int(idx)]
-        external_path = spec_info.get("prefix", None)
-        if external_path is not None:
-            external_path = spack.util.path.canonicalize_path(external_path)
-        self._specs[node].external_path = external_path
+        self._specs[node].external_path = spec_info.get("prefix", None)
         self._specs[node].external_modules = spack.spec.Spec._format_module_list(
             spec_info.get("modules", None)
         )
